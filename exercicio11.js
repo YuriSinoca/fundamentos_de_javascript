b9296d880c78d3c87e797305d97c05d02ecdda87
YuriSinoca <sinocayuri@gmail.com>
2025-01-08 15:53:25 -0300

    aula 03

---
+++ exercicio11.js
@@ -0,0 +1,30 @@
+//1. Crie um função chamada "calcularIdade" que recebe a idade e retorna o ano de nascimento
+//2. Crie uma função chamada "calcularMedia" que recebe tres notas e retorna a media 
+//3. Crie uma função chamada "verificarParImpar" que recebe um numero e retorna "Par" se o número for par, ou "Impar" se o número for impar
+
+//1.
+// let calcularIdade = function(n1){
+//     if(n1 > 2025){
+//         return "Isso é impossivel de ser calculado!"
+//     }
+//     return "Você tem ", n1, " e nasceu em", 2025 - n1 
+// }
+// console.log(calcularIdade(2026))
+
+//2.
+// let calcularMedia = function (n1, n2, n3) {
+//     let soma = n1 + n2 + n3
+//     let media = soma / 3
+//     if (media >= 6) {
+//         return `Você atingiu a media, sua media foi de , ${media}`
+//     } else if (media < 6) {
+//         return `Você não atingiu a media, sua media foi de , ${media}`
+//     }
+// }
+// console.log(calcularMedia(8, 9, 6))
+
+//3.
+// function verificarParImpar(numero){ 
+//     return numero % 2 == 0 ? "Par" : "Impar"
+// }
+//     console.log(verificarParImpar(3));
